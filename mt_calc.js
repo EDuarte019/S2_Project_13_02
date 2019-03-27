760400af89570b5339e395f117b682eab2d27590
Evelyn Duarte <eduarte019@west-mec.org>
2019-03-26 17:32:15 -0700

    Final Commit
    Made a switch key for the e.key and also declared attributes.

--- mt_calc.js
+++ mt_calc.js
@@ -37,7 +37,7 @@ window.onload = init;
 
 //the init function has a calcButton variabe that contains an object collection of calcButton. The for loop runs the buttonClick function when the click event happens. The calcKey runs when the keydown event happens.
 function init() {
-      var calcButtons = document.querySelectorAll("calcButton");
+      var calcButtons = document.getElementsByClassName("calcButton");
       for (var i = 0; i < calcButtons.length; i++) {
             calcButtons[i].onclick = buttonClick;
       }
@@ -45,22 +45,47 @@ function init() {
 }
 //The function buttonClick has a variable set to their class value. ButtonValue has it set to target the value. Also, a switch case that gives the value and the variable that is set to change.
 function buttonClick(e) {
-      var calcValue = document.getElementById("calcWindow");
-      var calcDecimal = document.getElementById("decimals");
+      var calcValue = document.getElementById("calcWindow").value;
+      var calcDecimal = document.getElementById("decimals").value;
       var buttonValue = e.target.value;
       switch (buttonValue) {
             case "del":
-                  calcValue += "";
+                  calcValue = "";
                   break;
             case "bksp":
-                  calcValue += eraseChar(calcValue);
+                  calcValue = eraseChar(calcValue);
+                  break;
             case "enter":
-                  calcValue +=
-                        " = " + evalEq(calcValue, calcDecimal) + "\n";
+                  calcValue += " = " + evalEq(calcValue, calcDecimal) + "\n";
+                  break;
             case "prev":
-                  calcValue += " = " + lastEq(calcValue) + "\n"
+                  calcValue += lastEq(calcValue) + "\n"
+                  break;
+            default:
+                  calcValue += buttonValue;
                   break;
       }
+      //the vaue is set to text area box calcValue. the focus puts the cursor focus within the calculator window.
+      document.getElementById("calcWindow").value = calcValue;
+      document.getElementById("calcWindow").focus();
+}
+// the function calcKeys has the same declaration as the function buttonClick. Then another switch case is placed for the event key except this case switch will prevent the default from happening.
+function calcKeys(e) {
+      var calcValue = document.getElementById("calcWindow").value;
+      var calcDecimal = document.getElementById("decimals").value;
+      switch (e.key) {
+            case "Delete":
+                  calcValue = "";
+                  break;
+            case "Enter":
+                  calcValue += " = " + evalEq(calcValue, calcDecimal);
+                  break;
+            case "ArrowUp":
+                  calcValue += lastEq(calcValue);
+                  e.preventDefault();
+      }
+      //The calcValue is equal to the value attribute of the calcWindow.
+      document.getElementById("calcWindow").value = calcValue;
 }
 
 
